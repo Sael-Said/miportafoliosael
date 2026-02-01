4605ea63e08f6c73637708c8708701848a0fbfe0
Sael-Said <avalossael@gmail.com>
2026-02-01 15:13:18 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Menu, X, Home, User, Code, Briefcase, Mail } from 'lucide-react';
 
 const Navbar = () => {
